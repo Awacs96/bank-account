d141a1080e8d73284db42d9792a883b517a6cf0e
Awacs <tomas.kosub@gmail.com>
2023-10-14 08:23:29 +0200

    basic contract logic implementation

--- contracts/BankAccount.sol
+++ contracts/BankAccount.sol
@@ -11,38 +11,100 @@ contract BankAccount {
 
     struct WithdrawRequest {
         address user;
-        uint amout;
+        uint amount;
         uint approvals;
         mapping(address => bool) ownersApproved;
         bool approved;
     }
 
     struct Account {
-        address[]owners;
+        address[] owners;
         uint balance;
         mapping(uint => WithdrawRequest) withdrawRequest;
     }
 
-    mapping(uint =>Account) accounts;
+    mapping(uint => Account) accounts;
     mapping(address => uint[]) userAccounts;
 
     uint nextAccountId;
     uint nextWithdrawId;
 
-    function deposit(uint accountId) external payable {
+    modifier isAccountOwner(uint accountId) {
+        bool isOwner;
 
+        for (uint i; i < accounts[accountId].owners.length; i++) {
+            if (accounts[accountId].owners[i] == msg.sender) {
+                isOwner = true;
+                break;
+            }
+        }
+
+        require(isOwner, "You may not deposit to an account your are not an owner of.");
+        _;
     }
 
-    function createAccount(address[] calldata owners) external {
+    modifier validOwners(address[] calldata owners) {
+        require(owners.length <= 3, "Each account can have a maximum of 4 owners.");
 
+        for(uint i; i < owners.length; i++) {
+            for (uint j = i + 1; j < owners.length; j++) {
+                if (owners[i] == owners[j] || owners[i] == msg.sender || owners[j] == msg.sender) {
+                    revert("No duplicate owners possible.");
+                }
+            }
+        }
+        _;
     }
 
-    function requestWithdrawl(uint accountId, uint amount) external {
-
+    modifier sufficientBalance(uint accountId, uint amount) {
+        require(accounts[accountId].balance >= amount, "Insufficient balance.");
+        _;
     }
 
-    function approveWithdrawl(uint accountId, uint withdrawId) external {
+    function deposit(uint accountId) external payable isAccountOwner(accountId) {
+        accounts[accountId].balance += msg.value;
+        emit Deposit(msg.sender, accountId, msg.value, block.timestamp);
+    }
 
+    function createAccount(address[] calldata otherOwners) external validOwners(otherOwners) {
+        address[] memory owners = new address[](otherOwners.length + 1);
+        owners[otherOwners.length] = msg.sender;
+
+        uint id = nextAccountId;
+
+        for (uint i; i < owners.length; i++) {
+            if (i < owners.length - 1) {
+                owners[i] = otherOwners[i];
+            }
+
+            if (userAccounts[owners[i]].length >= 3) {
+                revert("User can have a maximum of 3 accounts.");
+            }
+            userAccounts[owners[i]].push(id);
+        }
+
+        accounts[id].owners = owners;
+        nextAccountId++;
+        emit AccountCreated(owners, id, block.timestamp);
+    }
+
+    function requestWithdrawl(uint accountId, uint amount) external isAccountOwner(accountId) sufficientBalance(accountId, amount) {
+        uint id = nextWithdrawId;
+        WithdrawRequest storage request = accounts[accountId].withdrawRequest[id];
+        request.user = msg.sender;
+        request.amount = amount;
+        nextWithdrawId++;
+        emit WithdrawRequested(msg.sender, accountId, id, amount, block.timestamp);
+    }
+
+    function approveWithdrawl(uint accountId, uint withdrawId) external isAccountOwner(accountId) {
+        WithdrawRequest storage request = accounts[accountId].withdrawRequest[withdrawId];
+        request.approvals++;
+        request.ownersApproved[msg.sender] = true;
+
+        if (request.approvals == accounts[accountId].owners.length - 1) {
+            request.approved = true;
+        }
     }
 
     function withdraw(uint accountId, uint withdrawId) external {
